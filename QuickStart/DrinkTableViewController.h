cf40b1f1c4b4fb1a41d7a84d2fa9f78c489364a2
studentuser <studentuser@studentusers-MacBook-Pro-7.local>
2013-04-09 18:42:53 -0700

    Added DrinkTableView stuff

---
+++ QuickStart/DrinkTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrinkTableViewController.h
+//  QuickStart
+//
+//  Created by studentuser on 4/9/13.
+//  Copyright (c) 2013 Windows Azure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrinkTableViewController : UITableViewController
+
+@end
---
+++ QuickStart/DrinkTableViewController.m
@@ -0,0 +1,57 @@
+//
+//  DrinkTableViewController.m
+//  QuickStart
+//
+//  Created by studentuser on 4/9/13.
+//  Copyright (c) 2013 Windows Azure. All rights reserved.
+//
+
+#import "DrinkTableViewController.h"
+
+@interface DrinkTableViewController ()
+
+@end
+
+@implementation DrinkTableViewController
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+@end
