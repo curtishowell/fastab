e8a7c1129337bd9031c76523c56632893e34b9b3
curtishowell <github@curtishowell.net>
2013-05-11 21:34:01 -0700

    lots of changes and stuff

---
+++ QuickStart/OrderFulfillmentViewController.m
@@ -0,0 +1,38 @@
+//
+//  OrderFulfillmentViewController.m
+//  QuickStart
+//
+//  Created by Curtis Howell on 5/11/13.
+//  Copyright (c) 2013 Windows Azure. All rights reserved.
+//
+
+#import "OrderFulfillmentViewController.h"
+
+@interface OrderFulfillmentViewController ()
+
+@end
+
+@implementation OrderFulfillmentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
